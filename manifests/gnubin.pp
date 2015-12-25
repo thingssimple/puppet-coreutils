3377bedcddd9ff951a20922cde51cbf0df039fb4
Michael Avila <me@michaelavila.com>
2015-12-24 23:12:27 -0800

    Allow gnubin to be added to PATH/MANPATH

---
+++ manifests/gnubin.pp
@@ -0,0 +1,11 @@
+class coreutils::gnubin {
+  file_line { "Add gnubin to all environment paths":
+    path => "/etc/environment",
+    line => 'PATH="/opt/boxen/homebrew/opt/coreutils/libexec/gnubin:$PATH"'
+  }
+
+  file_line { "Add gnubin to all environment man paths":
+    path => "/etc/environment",
+    line => 'MANPATH="/opt/boxen/homebrew/opt/coreutils/libexec/gnuman:$MANPATH"'
+  }
+}
