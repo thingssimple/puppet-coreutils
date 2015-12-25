884324dae31b6640c43e00ca2a28a8d07a033b04
Michael Avila <me@michaelavila.com>
2015-12-24 23:24:50 -0800

    Simplify to add paths to zshrc for now (need to generalize this)

--- manifests/gnubin.pp
+++ manifests/gnubin.pp
@@ -1,15 +1,17 @@
 class coreutils::gnubin {
-  file { "/etc/environment":
+  $home = "/Users/${::boxen_user}"
+
+  file { "${home}/.zshrc":
     ensure => present
   }
 
   file_line { "Add gnubin to all environment paths":
-    path => "/etc/environment",
+    path => "${home}/.zshrc",
     line => 'PATH="/opt/boxen/homebrew/opt/coreutils/libexec/gnubin:$PATH"'
   }
 
   file_line { "Add gnubin to all environment man paths":
-    path => "/etc/environment",
+    path => "${home}/.zshrc",
     line => 'MANPATH="/opt/boxen/homebrew/opt/coreutils/libexec/gnuman:$MANPATH"'
   }
 }
